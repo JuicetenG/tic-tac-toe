cb2ece2e01076c85024925b7845bae87cd1994ba
JuicetenG <127890628+JuicetenG@users.noreply.github.com>
2024-04-30 19:50:55 -0400

    go through js to check for redundancies and comment for clarification

--- ttt.js
+++ ttt.js
@@ -1,5 +1,8 @@
+//creates and returns the board array
 const gameBoard = (function() {
   const board = [];
+
+  //fills board as 2D array
   function fillBoard() {
     let v = 1;
     for(let i = 0; i < 3; i++) {
@@ -12,8 +15,11 @@ const gameBoard = (function() {
     return board;
   }
 
+  //create board initially
   fillBoard();
   const getBoard = () => board;
+
+  //shows board for playing in console
   const showBoard = () => {
     console.log(board[0]);
     console.log(board[1]);
@@ -23,25 +29,33 @@ const gameBoard = (function() {
   return {
     showBoard,
     getBoard,
-    fillBoard,
+    fillBoard
   };
 })();
 
+//controls and updates the games display as well as handles click events
 const displayGame = (function () {
+  //variables needed for controlling screen
   const display = document.querySelector('#container');
   const playerDisplay = document.querySelector('#playerDisplay');
   const resetButton = document.querySelector('#reset');
-  const game = gameController();
   let gameEnd = false;
 
+  //initialize game
+  const game = gameController();
+
   const updateScreen = () => {
+    //reset screen
     display.textContent = '';
     
+    //get and display active player
     const player = game.getActivePlayer();
-    
     playerDisplay.textContent = `${player.name}'s turn`
+    
+    //create a variable for the board
     const board = gameBoard.getBoard();
 
+    //loop through 2d array and display the contents on the display board
     for(let i = 0; i < 3; i++) {
       const row = document.createElement('div');
       display.appendChild(row);
@@ -60,6 +74,7 @@ const displayGame = (function () {
       }
     }
 
+    //check for a winner and update display accordingly
     if(game.checkWinner()) {
       playerDisplay.textContent = `${player.name} Wins!`;
       gameEnd = true;
@@ -69,6 +84,7 @@ const displayGame = (function () {
     }
   }
 
+  //function for handling clicks on display board squares
   function playerClicks(e) {
     if(gameEnd === true) return;
     const clickedX = e.target.dataset.x;
@@ -77,6 +93,7 @@ const displayGame = (function () {
     updateScreen();
   }
 
+  //resets board, active player, and gameEnd variable to start new game
   function resetGame() {
     gameBoard.fillBoard();
     game.resetPlayer();
@@ -89,19 +106,22 @@ const displayGame = (function () {
   updateScreen();
 })(); 
 
+//controls the flow of the game
 function gameController() {
   const board = gameBoard.getBoard();
   const players = [
     {
-      name: 'Player 1',
+      name: 'Player X',
       value: 'X'
     },
     {
-      name: 'Player 2',
+      name: 'Player O',
       value: 'O'
     }
   ]
 
+  //the following lines set active player initially and create a method for
+  //changing, returning, and resetting that player
   let activePlayer = players[0];
   const changePlayer = () => {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
@@ -109,9 +129,12 @@ function gameController() {
   const getActivePlayer = () => activePlayer;
   const resetPlayer = () =>{ activePlayer = players[0] };
 
+  //display for console version of game
   console.log(`${activePlayer.name}: ${activePlayer.value}, Enter 2D array index using 2 numbers for selection.`);
   gameBoard.showBoard();
 
+  //takes active player and checks if board space is occupied, if not, places the active
+  //player's value on the board in the selected space. returns if the game finds a win or tie
   function playerMove(player, x, y) {
     if((board[x][y] !== 'X') && (board[x][y] !== 'O')) {
       board[x][y] = player.value;
@@ -121,6 +144,8 @@ function gameController() {
     } else return false;
   }
 
+  //checks for a winner by destructuring and comparing the winning nested array combinations
+  //in a forEach loop
   function checkWinner() {
     let win = false;
     const winningCombos = [
@@ -133,16 +158,18 @@ function gameController() {
       [[0,0], [1,1], [2,2]],
       [[0,2], [1,1], [2,0]],
     ]
-    
+
     winningCombos.forEach((combo) => {
       let [a, aa] = combo[0], [b, bb] = combo[1], [c, cc] = combo[2];
       if((board[a][aa] === board[b][bb] && board[b][bb] === board[c][cc])
-          && (board[a][aa] !== '' && board[b][bb] !== '' && board[c][cc] !== '')) win = true; 
+          && (board[a][aa] !== '' && board[b][bb] !== '' && board[c][cc] !== '')) {
+            win = true;
+          }   
     });
-
     return win;
   }
 
+  //checks that all spaces are occupied and checkWinner returns false for tie condition
   function checkTie() {
     let catsGame = 0;
     for(let i = 0; i < 3; i++) {
@@ -155,7 +182,11 @@ function gameController() {
     }
   }
 
+  //this is called when a player clicks on a square and it takes dataset attributes for the
+  //x and y which end up being the 2D array coordinates on the game board
   const playRound = (x, y) => {
+    //calls playerMove which returns true if the move is valid
+    //if the move is invalid it returns without changing players and no move is made
     if(playerMove(activePlayer, x, y)) {
       gameBoard.showBoard();
       changePlayer();
